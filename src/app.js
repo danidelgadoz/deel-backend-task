0a1fb73ff6e348dcde292dbb5969f3f84ec8ebeb
Daniel Delgado <dedd1993@gmail.com>
2022-06-03 18:24:25 -0500

    fix(contract): return the contract pnly if it belongs to the profile calling

--- src/app.js
+++ src/app.js
@@ -7,15 +7,8 @@ app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
-/**
- * FIX ME!
- * @returns contract by id
- */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
-    res.json(contract)
-})
+
+const contractRoutes = require( './routes/contract.routes');
+app.use('/contracts', contractRoutes);
+
 module.exports = app;
